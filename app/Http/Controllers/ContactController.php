6d97e9627d966c2ce884f89cf06ead1eec58abbe
prabhatb2000np <prabhatb2000np@gmail.com>
2015-05-14 18:19:42 +0200

    lession1 learned routing,controllers and views

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers;
+class ContactController extends Controller{
+    public function index(){
+        return view('forum.contact');
+    }
+    
+}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
---
+++ resources/views/forum/contact.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>
+            Contact Form
+        </title>
+    </head>
+    <body>
+    <h1>Contact Me!!!</h1>    
+    </body>    
+</html>
